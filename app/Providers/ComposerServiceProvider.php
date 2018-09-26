8f4dd2889f3c39cc4a2082dcbd0a1a2e41d6a5f2
Daniel Kong <kongdejia@gmail.com>
2018-09-26 16:46:27 -0700

    create composer service provider for view composer

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ComposerServiceProvider
+ *
+ * @package \App\Providers
+ */
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 当 Laravel 渲染 products.index 和 products.show 模板时，就会使用 CategoryTreeComposer 这个来注入类目树变量
+        // 同时 Laravel 还支持通配符，例如 products.* 即代表当渲染 products 目录下的模板时都执行这个 ViewComposer
+        // Using class based composers...
+        View::composer(
+            ['*'], 'App\Http\ViewComposers\CategoryTreeComposer'
+        );
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
