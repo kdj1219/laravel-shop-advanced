0a0c1df3a1789c1e358059219cb0342967aa2ddc
Daniel Kong <kongdejia@gmail.com>
2018-09-26 14:44:36 -0700

    商品类目管理后台

--- database/admin.sql
+++ database/admin.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
 --
--- Host: localhost    Database: laravel-shop
+-- Host: 127.0.0.1    Database: laravel-shop
 -- ------------------------------------------------------
--- Server version	5.7.21-0ubuntu0.16.04.1
+-- Server version	5.7.23-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-05-31 23:30:13'),(2,0,6,'系统管理','fa-tasks',NULL,NULL,'2018-06-05 07:33:59'),(3,2,7,'管理员','fa-users','auth/users',NULL,'2018-06-05 07:33:59'),(4,2,8,'角色','fa-user','auth/roles',NULL,'2018-06-05 07:33:59'),(5,2,9,'权限','fa-ban','auth/permissions',NULL,'2018-06-05 07:33:59'),(6,2,10,'菜单','fa-bars','auth/menu',NULL,'2018-06-05 07:33:59'),(7,2,11,'操作日志','fa-history','auth/logs',NULL,'2018-06-05 07:33:59'),(8,0,2,'用户管理','fa-users','/users','2018-05-31 23:54:14','2018-05-31 23:55:10'),(9,0,3,'商品管理','fa-cubes','/products','2018-06-01 00:28:19','2018-06-01 00:28:24'),(10,0,4,'订单管理','fa-rmb','/orders','2018-06-05 02:45:54','2018-06-05 02:46:10'),(11,0,5,'优惠券管理','fa-tags','/coupon_codes','2018-06-05 07:33:54','2018-06-05 07:33:59');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-05-31 23:30:13'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,'2018-09-26 21:28:25'),(3,2,8,'管理员','fa-users','auth/users',NULL,'2018-09-26 21:28:25'),(4,2,9,'角色','fa-user','auth/roles',NULL,'2018-09-26 21:28:25'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,'2018-09-26 21:28:25'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,'2018-09-26 21:28:25'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,'2018-09-26 21:28:25'),(8,0,2,'用户管理','fa-users','/users','2018-05-31 23:54:14','2018-05-31 23:55:10'),(9,0,4,'商品管理','fa-cubes','/products','2018-06-01 00:28:19','2018-09-26 21:28:25'),(10,0,5,'订单管理','fa-rmb','/orders','2018-06-05 02:45:54','2018-09-26 21:28:25'),(11,0,6,'优惠券管理','fa-tags','/coupon_codes','2018-06-05 07:33:54','2018-09-26 21:28:25'),(12,0,3,'类目管理','fa-bars','/categories','2018-09-26 21:28:15','2018-09-26 21:28:25');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -41,7 +41,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_role_menu` WRITE;
 /*!40000 ALTER TABLE `admin_role_menu` DISABLE KEYS */;
-INSERT INTO `admin_role_menu` VALUES (1,2,NULL,NULL);
+INSERT INTO `admin_role_menu` VALUES (1,2,NULL,NULL),(1,10,NULL,NULL),(1,11,NULL,NULL);
 /*!40000 ALTER TABLE `admin_role_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,7 +51,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_role_permissions` WRITE;
 /*!40000 ALTER TABLE `admin_role_permissions` DISABLE KEYS */;
-INSERT INTO `admin_role_permissions` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL),(2,3,NULL,NULL),(2,4,NULL,NULL),(2,6,NULL,NULL),(2,7,NULL,NULL),(2,8,NULL,NULL),(2,9,NULL,NULL);
+INSERT INTO `admin_role_permissions` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL),(2,3,NULL,NULL),(2,4,NULL,NULL),(2,7,NULL,NULL);
 /*!40000 ALTER TABLE `admin_role_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,6 +81,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_user_permissions` WRITE;
 /*!40000 ALTER TABLE `admin_user_permissions` DISABLE KEYS */;
+INSERT INTO `admin_user_permissions` VALUES (2,7,NULL,NULL);
 /*!40000 ALTER TABLE `admin_user_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +91,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_users` WRITE;
 /*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
-INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$OznUPzz7OoiDZ/l4/qS1HeUULZUt20z7gxnHD8t5SIVHJK/kgINES','Administrator',NULL,'T0Z0y7MHmNNjw0r8NvnOmOLL4tKg4GGUziYtjYOoabrzhuTLs1JWEO1QRjuZ','2018-05-31 23:27:29','2018-05-31 23:27:29'),(2,'operator','$2y$10$YcmXJVEVHFAERGt14fjM6.IHDKdUEuNRhRjuYAZ39qoRBD9g29I8q','运营',NULL,'miIX5uwGont5AjaGsu84n2iMnzJLONPmYpmtb0Qhe7dgvgFgiJp52VpMxNSO','2018-06-01 00:13:51','2018-06-01 00:13:51');
+INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$OznUPzz7OoiDZ/l4/qS1HeUULZUt20z7gxnHD8t5SIVHJK/kgINES','Administrator',NULL,'K3YYMTuW3xp5QzDdbiPoxL7NXoXsEFTIdkILAuVMTBSlUhUddnPaikN8MaxC','2018-05-31 23:27:29','2018-05-31 23:27:29'),(2,'operator','$2y$10$HJgKW80gpCP1s1pG3XEP7.It0SVwOmJQPtrIZmLQe5SKf26.iDk86','运营','images/qianqian_ avatar.jpg','QoYnOaHkAyCBlvefW4E0vKnFcQvZERv4jIhP65WVnqPynzQa2CQXwYFyfBpi','2018-06-01 00:13:51','2018-09-22 00:12:36');
 /*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -103,4 +104,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-05 22:46:26
+-- Dump completed on 2018-09-26 14:43:53
